68db35b51197422118dd50686716df8dde3d3dbb
Devin DiNapoli <devindinapoli@gmail.com>
2018-05-24 18:38:52 -0400

    Added logic to grab items from api in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './services/todo.service';
+import { Response } from '@angular/http';
+import ToDo from './models/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(
+
+    private todoService: TodoService
+  ) { }
+
+  public newTodo: ToDo = new ToDo();
+
+  todosList: ToDo[];
+
+  ngOnInit(): void {
+
+    this.todoService.getToDos()
+    .subscribe(todos => {
+
+      this.todosList = todos;
+      console.log(todos);
+    });
+  }
 }
