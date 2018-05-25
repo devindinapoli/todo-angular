1bf10512bc2cf492c862b607b1c4d1eca7b8de12
Devin DiNapoli <devindinapoli@gmail.com>
2018-05-24 20:45:04 -0400

    app works, need to fix CORS issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent implements OnInit {
   public newTodo: ToDo = new ToDo();
 
   todosList: ToDo[];
+  editTodos: ToDo[] = [];
 
   ngOnInit(): void {
 
@@ -26,6 +27,53 @@ export class AppComponent implements OnInit {
 
       this.todosList = todos;
       console.log(todos);
+    });
+  }
+
+  create() {
+    this.todoService.createTodo(this.newTodo)
+    .subscribe((res) => {
+      this.todosList.push(res.data);
+      this.newTodo = new ToDo();
+    });
+  }
+
+  editTodo(todo: ToDo) {
+    console.log(todo);
+    if (this.todosList.includes(todo)) {
+      if (!this.editTodos.includes(todo)) {
+        this.editTodos.push(todo);
+      } else {
+        this.editTodos.splice(this.editTodos.indexOf(todo), 1);
+        this.todoService.editToDo(todo).subscribe(res => {
+          console.log('Successfully Updated');
+        }, err => {
+          this.editTodo(todo);
+          console.error('Could Not Update');
+        });
+      }
+    }
+  }
+
+  doneTodo(todo: ToDo) {
+    todo.status = 'Done';
+    this.todoService.editToDo(todo).subscribe(res => {
+      console.log('Succesfully Updated');
+    }, err => {
+      this.editTodo(todo);
+      console.error('Could Not Update');
+    });
+  }
+
+  submitTodo(event, todo: ToDo) {
+    if (event.keyCode === 13) {
+      this.editTodo(todo);
+    }
+  }
+
+  deleteTodo(todo: ToDo) {
+    this.todoService.deleteTodo(todo._id).subscribe(res => {
+      this.todosList.splice(this.todosList.indexOf(todo), 1);
     });
   }
 }
